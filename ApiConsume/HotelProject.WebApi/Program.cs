d23f8d62fac078cfc9d444b620551ad6556aede0
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-06-19 11:03:23 +0300

    Swagger işlemi diğer controllerlar  içinde gerçekleştirildi ve test edildi.

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -15,6 +15,14 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Context>();
 builder.Services.AddScoped<IStaffDal,EFStaffDal>();
 builder.Services.AddScoped<IStaffService,StaffManager>();
+builder.Services.AddScoped<IServiceDal, EFServiceDal>();
+builder.Services.AddScoped<IServiceService, ServiceManager>();
+builder.Services.AddScoped<IRoomDal, EFRoomDal>();
+builder.Services.AddScoped<IRoomService, RoomManager>();
+builder.Services.AddScoped<ISubscribeDal, EFSubscribeDal>();
+builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+builder.Services.AddScoped<ITestimonialDal, EFTestimonialDal>();
+builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 
 var app = builder.Build();
 
