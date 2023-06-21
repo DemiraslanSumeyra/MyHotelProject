9d5547eb91d389950203063677f0cd0676eecdb0
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-06-21 11:17:32 +0300

    ServiceDto ları oluşturuldu.

---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,16 @@
+﻿using HotelProject.WebUI.Models;
+using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class CreateServiceDto
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage="Lütfen servis ikon linkini giriniz")]
+        public string ServiceIcon { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen hizmet başlığı  giriniz")]
+        [StringLength(100,ErrorMessage ="Lütfen en fazla 100 karakter veri girişi yapınız")]
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit.Sdk;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+        public int ServiceID { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen servis ikon linkini giriniz")]
+        public string ServiceIcon { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen hizmet başlığı  giriniz")]
+        [StringLength(100, ErrorMessage = "Lütfen en fazla 100 karakter veri girişi yapınız")]
+        public string Title { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen hizmet açıklaması  giriniz")]
+        [StringLength(500, ErrorMessage = "Lütfen en fazla 500 karakter veri girişi yapınız")]
+        public string Description { get; set; }
+    }
+}
