73ee8d5331ef6b2e628a6f5d15ae972ad7f89d63
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-06-22 12:41:51 +0300

    LoginController oluşturuldu ve login olma işlemi gerçekleştirildi.

---
+++ Frontend/HotelProject.WebUI/Dtos/LoginDto/LoginUserDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using Xunit.Sdk;
+
+namespace HotelProject.WebUI.Dtos.LoginDto
+{
+    public class LoginUserDto
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage="Lütfen kullanıcı adını giriniz")]
+        public string Username { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen şifreyi giriniz")]
+        public string Password { get; set; }
+    }
+}
