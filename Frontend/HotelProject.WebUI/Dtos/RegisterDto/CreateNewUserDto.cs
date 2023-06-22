e0d7aafb0548df14bc298c10a20bd5f2ad570caa
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-06-22 12:12:53 +0300

    Register Dto oluşturuldu ve register işlemi gerçekleştirildi.

---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen adı giriniz")]
+        public string Name { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage="Lütfen soyadı giriniz")]
+        public string Surname { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen şehri giriniz")]
+        public string City { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen kullanıcı adını giriniz")]
+        public string Username { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen maili giriniz")]
+        public string Mail { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen şifreyi giriniz")]
+        public string Password { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz")]
+        [Compare("Password",ErrorMessage ="Şifreler birbiri ile uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+    }
+}
