4523d8d40756bd6ace07e2b1e30a33cf1d415449
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-07-03 15:18:24 +0300

    Girilen şehir değerine göre otel lokasyonu getirildi.

--- RapidApi/RapidApiConsume/Controllers/SearchLocationIDController.cs
+++ RapidApi/RapidApiConsume/Controllers/SearchLocationIDController.cs
@@ -6,27 +6,53 @@ namespace RapidApiConsume.Controllers
 {
     public class SearchLocationIDController : Controller
     {
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string cityName)
         {
-            List<ApiBookingLocationSearchViewModel> model = new List<ApiBookingLocationSearchViewModel>(); 
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
+            if (!string.IsNullOrEmpty(cityName))
             {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Berlin&locale=en-gb"),
-                Headers =
+                List<ApiBookingLocationSearchViewModel> model = new List<ApiBookingLocationSearchViewModel>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com.p.rapidapi.com/v1/hotels/locations?name={cityName}&locale=en-gb"),
+                    Headers =
     {
         { "X-RapidAPI-Key", "8f2fcf637bmsh898d1e8f3247637p1cce8cjsn71991ae9a090" },
         { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
     },
-            };
-            using (var response = await client.SendAsync(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-                model = JsonConvert.DeserializeObject<List<ApiBookingLocationSearchViewModel>>(body);
-                return View(model.Take(1).ToList());
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<List<ApiBookingLocationSearchViewModel>>(body);
+                    return View(model.Take(1).ToList());
+                }
             }
+            else
+            {
+                List<ApiBookingLocationSearchViewModel> model = new List<ApiBookingLocationSearchViewModel>();
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Berlin&locale=en-gb"),
+                    Headers =
+    {
+        { "X-RapidAPI-Key", "8f2fcf637bmsh898d1e8f3247637p1cce8cjsn71991ae9a090" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<List<ApiBookingLocationSearchViewModel>>(body);
+                    return View(model.Take(1).ToList());
+                }
+            }
+            
         }
     }
 }
