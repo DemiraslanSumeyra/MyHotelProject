595b2d415d71987606cb3c07a127cca80c90f2bc
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-07-03 14:53:54 +0300

    SearchLocationID api ile otel bilgisinin lokasyonu alındı.

---
+++ RapidApi/RapidApiConsume/Controllers/SearchLocationIDController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class SearchLocationIDController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ApiBookingLocationSearchViewModel> model = new List<ApiBookingLocationSearchViewModel>(); 
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/locations?name=Berlin&locale=en-gb"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "8f2fcf637bmsh898d1e8f3247637p1cce8cjsn71991ae9a090" },
+        { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                model = JsonConvert.DeserializeObject<List<ApiBookingLocationSearchViewModel>>(body);
+                return View(model.Take(1).ToList());
+            }
+        }
+    }
+}
