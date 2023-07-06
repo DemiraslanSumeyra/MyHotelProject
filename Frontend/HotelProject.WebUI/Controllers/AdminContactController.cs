78eb70864bb4f0710d2f4868592c90089f66abab
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-07-06 13:56:51 +0300

    Gönderilen mesajların listelenmesi işlemi gerçekleştirildi.

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -28,6 +28,18 @@ namespace HotelProject.WebUI.Controllers
             }
             return View();
         }
+        public async Task<IActionResult> Sendbox()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:45181/api/SendMessage");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSendboxDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
         [HttpGet]
         public IActionResult AddSendMessage()
         {
