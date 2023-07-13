100f88a3bafc3ae52275c6120890cccbff69a228
Sumeyra Demiraslan <demiraslansumeyra34@gmail.com>
2023-07-13 12:15:03 +0300

    AdminUsers sayfası oluşturuldu ve kullanıcı listeleme işlemi gerçekleştirildi.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20230713083627_mig_add_imageUrl.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    public partial class mig_add_imageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageURL",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageURL",
+                table: "AspNetUsers");
+        }
+    }
+}
